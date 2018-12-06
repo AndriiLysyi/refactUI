ff908d3afba6c045babce24501f7d17f9631006d
DESKTOP-2944B7L\Ivan <gulldesert@gmail.com>
2018-12-06 20:35:35 +0200

    One/two editors

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -8,16 +8,42 @@ declare let ace: any;
 })
 export class CodeEditorComponent implements AfterViewInit {
   myCode = 'for(var i=0; i<100; i++) {\nfunction(variable){\nt}\n\n}';
+  errorResponse = "brackets are not closed\n\n\n\n\n\n\n\n\n\nthat's all";
+  isResultingPage = false;
+
   @ViewChild('codeEditor') codeEditor;
+  @ViewChild('errorEditor') errorEditor;
 
   onRuleChange(e) {
     console.log(e.toString());
   }
 
-  ngAfterViewInit() {
-    //const Range = ace.require('ace/range')['Range'];
+  ngAfterViewInit() {    
     const editor = this.codeEditor.getEditor();
-    editor.getSession().setMode("ace/mode/javascript");   
+    editor.setShowPrintMargin(false);    
+    editor.getSession().setMode("ace/mode/javascript");  
   }
 
+  initErrorEditor() {
+    const editor = this.codeEditor.getEditor();
+
+    const responseEditor = this.errorEditor.getEditor();
+    responseEditor.renderer.setShowGutter(false);    
+    responseEditor.getSession().setMode("ace/mode/text");
+
+    //synchronous scrolling
+    editor.getSession().on('changeScrollTop', function(scroll) {
+      responseEditor.getSession().setScrollTop(parseInt(scroll) || 0)
+    });
+    responseEditor.getSession().on('changeScrollTop', function(scroll) {
+      editor.getSession().setScrollTop(parseInt(scroll) || 0)
+    });
+    return true;
+  }
+
+  sendAnswer() {
+    this.isResultingPage = true;
+    setTimeout(()=>this.initErrorEditor(), 0);
+    
+  }
 }
