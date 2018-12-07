533d793c646d9a060deed2dd90b0b17924fc5102
AndriiLysyi <andrii.lysyi1@gmail.com>
2018-12-07 15:39:51 +0200

    markup

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,14 +9,14 @@ import { ProfileEditorComponent } from './profile-editor';
 import { ChartComponent } from './chart';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', component: ProfileEditorComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'editor', component: CodeEditorComponent, canActivate: [AuthGuard] },
     { path: 'profile', component: ProfileEditorComponent, canActivate: [AuthGuard]},
     { path: 'stat', component: ChartComponent, canActivate: [AuthGuard] },
     // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'stat' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
